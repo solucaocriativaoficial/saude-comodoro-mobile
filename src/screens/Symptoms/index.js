192ea504a7a0af3a999a88aff562c33dded70be4
brunolgb <brunodscolado@gmail.com>
2020-03-26 11:37:23 -0400

    add screens de sintomas

---
+++ src/screens/Symptoms/index.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react'
+import {View, Text, FlatList} from 'react-native'
+import Style from './style'
+import api from '../../api'
+
+import Header from '../../components/header'
+
+export default function Symptoms({navigation}){
+    const [symptoms, setSymptoms] = useState([])
+    const [empty, setEmpty] = useState('Carregando')
+    useEffect(() => {
+        async function getContent(){
+            const responseRequest = await api.get('./symptoms')
+            const {status, data} = responseRequest
+            if(status === 200 && data.success === true)
+            setSymptoms(data.content)
+
+            if(!data.success)
+            setEmpty(data.message)
+        }
+        getContent()
+    }, [])
+    if(!symptoms.length)
+    return(
+        <>
+        <Header navigation={navigation} title="Sintomas"/>
+        <View style={Style.container}>
+            <View style={Style.box_empty}>
+                <Text style={Style.empty}>{empty}</Text>
+            </View>
+        </View>
+        </>
+    )
+
+    return(
+        <>
+        <Header navigation={navigation} title="Sintomas"/>
+        <View style={Style.container}>
+            <FlatList
+                data={symptoms}
+                renderItem={
+                    ({item}) => 
+                    <View style={Style.post}>
+                            <Text style={Style.info}>{item.symptoms}</Text>
+                    </View>}
+                    keyExtractor={item => item._id}
+            />
+        </View>
+        </>
+    )
+}
---
+++ src/screens/Symptoms/style.js
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native'
+import StylePattern from '../../components/StylePattern'
+const style = StyleSheet.create({
+    container: {
+        flex:1,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    box_empty:{
+        height: 100,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    empty: {
+        color: StylePattern.colors.black_light,
+        fontSize: 14,
+        fontWeight: "bold"
+    },
+    post: {
+        flex: 1,
+        marginVertical: 10,
+        justifyContent: 'center'
+    },
+    info:{
+        fontSize: 16,
+        fontWeight: "bold",
+        color: StylePattern.colors.secundary
+    }
+})
+
+export default style
